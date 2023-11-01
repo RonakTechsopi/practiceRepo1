ea2d03bb778a9cc6013c50fc0d122303f7a2cc59
RonakTechsopi <ronak.tinker@techsopi.com>
2023-11-01 17:02:49 +0530

    templete driven form in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularTempleteDrivenForm';
+  countryList:Country[]=[
+    new Country("1","India"),
+    new Country("2","England"),
+    new Country("3","South Africa")
+  ]
+
+  onSubmit(contactForm:any){
+    console.log(contactForm.value);
+  }
+
+}
+
+export class Country{
+  id;
+  name;
+
+  constructor(id:String,name:String){
+    this.id=id;
+    this.name=name;
+  }
 }
